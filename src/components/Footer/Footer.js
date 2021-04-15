20a19f2d95711d05d54cd5280977fe0e5ee4740b
Parth Sharma <parthsharma151@gmail.com>
2021-04-16 04:30:33 +0530

    edit: footer and image

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Footer = () => {
     return(
         <div>
-            <div>
+            <div style={
+                {
+                    fontSize: "24px",
+                    color: "gray",
+                    marginTop: "80px",
+                }
+            }>
                 Made with love by @psrth
             </div>
         </div>
